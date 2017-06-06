d6b8be8d519c1e4dc07e0126afd79c2bf7f03347
shivagajjala09@gmail.com <shiva.gajjala9@gmail.com>
2017-06-06 17:57:59 -0500

    spring-boot-heroku

---
+++ Springboot-Heroku/src/main/java/com/example/demo/SpringbootHerokuApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootHerokuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootHerokuApplication.class, args);
+	}
+}
